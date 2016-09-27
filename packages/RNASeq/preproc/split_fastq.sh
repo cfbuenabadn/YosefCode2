300cafb0f6d0a50915f72e43a2ae8d4bed1ac95d
xiuweiz <zhangxiuwei03@gmail.com>
2016-09-26 17:20:49 -0700

    Update split_fastq.sh

--- packages/RNASeq/preproc/split_fastq.sh
+++ packages/RNASeq/preproc/split_fastq.sh
@@ -1,3 +1,14 @@
 # split fastq files
-grep -A 3 \ 1:N S0097.fastq | sed '/^--$/d' > S0097_1.fastq
-grep -A 3 \ 2:N S0097.fastq | sed '/^--$/d' > S0097_2.fastq
+#!/bin/bash
+
+# 97-99
+# 100-385
+CELL=100
+while [  $CELL -lt 385 ]; do
+gunzip S0$CELL.fastq.gz grep -A 3 \ 1:N | sed '/^--$/d' > /data/yosef2/users/xiuwei/fastq/S0$CELL_1.fastq
+gunzip S0$CELL.fastq.gz grep -A 3 \ 2:N | sed '/^--$/d' > /data/yosef2/users/xiuwei/fastq/S0$CELL_2.fastq
+let CELL=CELL+1 
+done
+         
+
+
