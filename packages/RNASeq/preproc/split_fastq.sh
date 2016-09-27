e85125d4c3253a1db66839d01b1353ddf4876844
xiuweiz <zhangxiuwei03@gmail.com>
2016-09-26 17:37:30 -0700

    Update split_fastq.sh

--- packages/RNASeq/preproc/split_fastq.sh
+++ packages/RNASeq/preproc/split_fastq.sh
@@ -2,11 +2,18 @@
 #!/bin/bash
 
 # 97-99
-# 100-385
-CELL=100
-while [  $CELL -lt 385 ]; do
-gunzip S0$CELL.fastq.gz grep -A 3 \ 1:N | sed '/^--$/d' > /data/yosef2/users/xiuwei/fastq/S0$CELL_1.fastq
-gunzip S0$CELL.fastq.gz grep -A 3 \ 2:N | sed '/^--$/d' > /data/yosef2/users/xiuwei/fastq/S0$CELL_2.fastq
+
+for CELL in {97..99} do
+gunzip S00$CELL.fastq.gz
+grep -A 3 \ 1:N S00$CELL.fastq | sed '/^--$/d' > /data/yosef2/users/xiuwei/fastq/S00$CELL_1.fastq
+grep -A 3 \ 2:N S00$CELL.fastq | sed '/^--$/d' > /data/yosef2/users/xiuwei/fastq/S00$CELL_2.fastq
+let CELL=CELL+1 
+done
+
+for CELL in {100..384} do
+gunzip S0$CELL.fastq.gz
+grep -A 3 \ 1:N S0$CELL.fastq | sed '/^--$/d' > /data/yosef2/users/xiuwei/fastq/S0$CELL_1.fastq
+grep -A 3 \ 2:N S0$CELL.fastq | sed '/^--$/d' > /data/yosef2/users/xiuwei/fastq/S0$CELL_2.fastq
 let CELL=CELL+1 
 done
          
